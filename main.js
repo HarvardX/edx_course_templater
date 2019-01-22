55bfd74bbd0f0b744df9e803c2f4b3cccdbf96f6
Colin-Fredericks <colin.fredericks@gmail.com>
2019-01-22 14:17:10 -0500

    fiddling

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function makeDownload() {
     tar.addFile(file_source.substring(file_source.lastIndexOf('/')+1), file_source);
 
     let download_link = document.createElement('a');
-    download_link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(tar.write()) );
+    download_link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(tar) );
     download_link.setAttribute('download', filename);
 
     let download_text = 'Click here to download.';
