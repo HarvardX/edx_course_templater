5eb921758dd3939c4cd452b7f549fa18e7cbb71e
Colin-Fredericks <colin.fredericks@gmail.com>
2019-01-22 13:53:07 -0500

    tarball.js test

--- main.js
+++ main.js
@@ -5,10 +5,13 @@
 function makeDownload() {
 
     let filename = document.getElementById('filename').value;
-    let filetext = document.getElementById('filetext').value;
+    let file_source = document.getElementById('filesource').value;
+
+    let tar = new tarball.TarWriter();
+    tar.addFile(file_source.substring(file_source.lastIndexOf('/')+1), file_source);
 
     let download_link = document.createElement('a');
-    download_link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(filetext));
+    download_link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(tar.write());
     download_link.setAttribute('download', filename);
 
     let download_text = 'Click here to download.';
