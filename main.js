757bc83a907b141c4f6696fc72f8080ad3610696
Colin-Fredericks <colin.fredericks@gmail.com>
2019-01-31 18:59:01 -0500

    Progress in integrating sides

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     constructCourseTemplate();
     $('input').on('change', function(e){
         // console.log(e);
-        constructCourseTemplate();
+        console.log(constructCourseTemplate());
     });
 
     $('input[name="corecontent"]').on('change', function(){
@@ -48,13 +48,6 @@ function constructCourseTemplate(){
     //  'text': 'inner xml for file'
     // }
     let template = [];
-
-    let courseXML = $(`<course advanced_modules="[&quot;openassessment&quot;, &quot;word_cloud&quot;, &quot;lti_consumer&quot;, &quot;split_test&quot;, &quot;library_content&quot;, &quot;poll&quot;, &quot;survey&quot;, &quot;ubcpi&quot;, &quot;drag-and-drop-v2&quot;, &quot;done&quot;]" cert_html_view_enabled="true" display_name="HarvardX Boilerplate" language="en" start="&quot;2030-01-01T00:00:00+00:00&quot;">
-  <chapter url_name="ae4d34a5898348ec8f02796adfd3c211"/>
-  <chapter url_name="bc94ec689194454dbc8148b3e53fc80c"/>
-  <chapter url_name="cceae70553594a3dbdd65da2d9e7fd11"/>
-  <wiki slug="HarvardX.HX102.3T2018"/>
-</course>`);
 
     let sections = $('#numsections').val();
     let subsections = $('#numsubsections').val();
@@ -95,7 +88,7 @@ function constructCourseTemplate(){
                 if(vid_has_intro){
                     if(coreTag == 'video'){
                         let vhti_file = 's_' + (s+1) + '_ss_' + (ss+1) + '_p_' + (p+1) + '_vidintro.xml';
-                        vertical_innards += '<html url_name=' + vhti_file + '>\n';
+                        vertical_innards += '<html url_name="' + vhti_file + '" />\n';
                         template.push({
                             'path': 'html/' + vhti_file,
                             'text': '<html>\n</html>'
@@ -110,7 +103,7 @@ function constructCourseTemplate(){
                 // add content page tags
                 for(let cf = 0; cf < numCoreElements; cf++){
                     let core_file = 's_' + (s+1) + '_ss_' + (ss+1) + '_p_' + (p+1) + '_' + coreTag + '_' + (cf+1) + '.xml'
-                    vertical_innards += '<' + coreTag + ' url_name=' + core_file + '>\n';
+                    vertical_innards += '<' + coreTag + ' url_name="' + core_file + '" />\n';
                     template.push({
                         'path': coreTag + '/' + core_file,
                         'text': '<' + coreTag +'>\n</' + coreTag + '>'
@@ -125,7 +118,7 @@ function constructCourseTemplate(){
 
                 if(prob_on_every_page){
                     let poep_file = 's_' + (s+1) + '_ss_' + (ss+1) + '_p_' + (p+1) + '_problem_x.xml';
-                    vertical_innards += '<problem url_name=' + poep_file + '>\n';
+                    vertical_innards += '<problem url_name="' + poep_file + '" />\n';
                     template.push({
                         'path': 'problem/' + poep_file,
                         'text': '<problem>\n</problem>'
@@ -136,7 +129,7 @@ function constructCourseTemplate(){
                 if(disc_on_every_page){
                     if(disc_has_intro){
                         let dhti_file = 's_' + (s+1) + '_ss_' + (ss+1) + '_p_' + (p+1) + '_discintro.xml';
-                        vertical_innards += '<html url_name=' + dhti_file + '>\n';
+                        vertical_innards += '<html url_name="' + dhti_file + '" />\n';
                         template.push({
                             'path': 'html/' + dhti_file,
                             'text': '<html>\n</html>'
@@ -147,7 +140,7 @@ function constructCourseTemplate(){
                         });
                     }
                     let doep_file = 's_' + (s+1) + '_ss_' + (ss+1) + '_p_' + (p+1) + '_problem_x.xml';
-                    vertical_innards += '<discussion url_name=' + doep_file + ' xblock-family="xblock.v1" discussion_category="Chapter ' + (s+1) + '">\n'
+                    vertical_innards += '<discussion url_name="' + doep_file + '" xblock-family="xblock.v1" discussion_category="Chapter ' + (s+1) + '" />\n'
                     // no need to add to template, only declared inline.
                 }
 
@@ -155,9 +148,9 @@ function constructCourseTemplate(){
                 let vert_file = 's_' + (s+1) + '_ss_' + (ss+1) + '_p_' + (p+1) + '.xml';
                 template.push({
                     'path': 'vertical/' + vert_file,
-                    'text': '<vertical>\n' + vertical_innards + '\n</vertical>'
+                    'text': '<vertical>\n' + vertical_innards + '</vertical>'
                 });
-                sequential_innards += '<vertical url_name=' + vert_file + '/>\n';
+                sequential_innards += '  <vertical url_name="' + vert_file + '" />\n';
             }
 
             // add tags for subsection header page
@@ -176,19 +169,19 @@ function constructCourseTemplate(){
             let seq_file = 's_' + (s+1) + '_ss_' + (ss+1) + '.xml';
             template.push({
                 'path': 'sequential/' + seq_file,
-                'text': '<sequential>\n' + sequential_innards + '\n</sequential>'
+                'text': '<sequential>\n' + sequential_innards + '</sequential>'
             });
-            chapter_innards += '<sequential url_name=' + seq_file + '/>';
+            chapter_innards += '  <sequential url_name="' + seq_file + '" />\n';
         }
         // add chapter tag to template
         template.push({
-            'path': 'chapter/' + (s+1) + '.xml',
-            'text': '<chapter>\n' + chapter_innards + '\n</chapter>'
+            'path': 'chapter/s_' + (s+1) + '.xml',
+            'text': '<chapter>\n' + chapter_innards + '</chapter>'
         });
 
     }
 
-    console.log(template);
+    return template;
 }
 
 
@@ -210,18 +203,48 @@ function readCourseFlatFile(filepath, callback){
 
 }
 
+async function makeNewCourseXML(template){
 
-async function makeTarFromFlatFile(f, path, makeDownloadLink){
+    // console.log(template);
+
+    let new_chapters = template.filter(function(row){
+        return row.path.slice(0,7) === 'chapter';
+    });
+
+    console.log(new_chapters);
+
+    let course_xml = '<course advanced_modules="[&quot;openassessment&quot;, &quot;word_cloud&quot;, &quot;lti_consumer&quot;, &quot;split_test&quot;, &quot;library_content&quot;, &quot;poll&quot;, &quot;survey&quot;, &quot;ubcpi&quot;, &quot;drag-and-drop-v2&quot;, &quot;done&quot;]" cert_html_view_enabled="true" display_name="HarvardX Boilerplate" language="en" start="&quot;2030-01-01T00:00:00+00:00&quot;">\n';
+    course_xml += '  <chapter url_name="ae4d34a5898348ec8f02796adfd3c211"/>\n';
+
+    for(let i = 0; i < new_chapters.length; i++){
+        // Cutting off course/ and .xml from path.
+        course_xml += '  <chapter url_name="' + new_chapters[i].path.slice(8,-4) + '" />\n'
+    }
+
+    course_xml += '  <chapter url_name="bc94ec689194454dbc8148b3e53fc80c"/>\n'
+    course_xml += '  <chapter url_name="cceae70553594a3dbdd65da2d9e7fd11"/>\n'
+    course_xml += '  <wiki slug="HarvardX.HX102.3T2018"/>\n'
+    course_xml += '</course>'
+
+    console.log(course_xml);
+    return course_xml;
+}
+
+async function makeTarFromFlatFile(f, path, template, makeDownloadLink){
     let textlines = f.split('\n');
     console.log('Making course tarball from...');
-    console.log(textlines);
+    // console.log(textlines);
 
     let tar = new tarball.TarWriter();
     let filecounter = 0;
 
+    template.forEach((temp_row) => {
+        tar.addTextFile(temp_row.path, temp_row.text);
+    });
+
     textlines.forEach(async function(row){
 
-        // console.log(row);
+        console.log(row);
 
         // Taking off the ./ from start of each. Artifact of using "find" command.
         if(row.slice(0,2) == './'){
@@ -237,10 +260,15 @@ async function makeTarFromFlatFile(f, path, makeDownloadLink){
                 .then(res => res.blob())
                 .then(blob => {
                     // console.log('blob obtained');
+                    // console.log(row);
                     // console.log(blob);
-                    // Sweet functionality note: folders are added automatically
-                    // because the row text has the folder name and a slash.
-                    tar.addFile(row, blob);
+                    if(row.slice(0,7) == 'course/'){
+                        tar.addTextFile(row, makeNewCourseXML(template));
+                    }else{
+                        // Sweet functionality note: folders are added automatically
+                        // because the row text has the folder name and a slash.
+                        tar.addFile(row, blob);
+                    }
                     filecounter++;
             });
         }
@@ -261,17 +289,19 @@ async function makeDownload() {
     let target_location = $('#dlink');
     let download_placeholder = $('<p>Creating tar file...</p>');
     let filename = $('#filename').val();
-    let file_source = $('#filesource').val();
     target_location.append(download_placeholder);
 
-    // We're just downloading the boilerplate tarball right now.
-    let boilerplate_path = "https://harvardx.github.io/edx_course_templater/boilerplate_course/";
-    let boilerplate_structure_path = "https://harvardx.github.io/edx_course_templater/boilerplate_course.txt";
+    // Get the parts of the template that aren't boilerplate.
+    let template = constructCourseTemplate();
+
+    // Path to the boilerplate parts of the template.
+    let boilerplate_structure_file = $('#filesource').val();
+    let boilerplate_structure_path = boilerplate_structure_file.slice(0,-4)+'/';
     let course_tarball;
 
 
     // Get the flat file that describes the boilerplate course.
-    let boilerplate_flat = await readCourseFlatFile(file_source, async function(result){
+    let boilerplate_flat = await readCourseFlatFile(boilerplate_structure_file, async function(result){
 
         // console.log('course structure:');
         // console.log(result);
@@ -293,7 +323,7 @@ async function makeDownload() {
         }
 
         // Construct a tarball from the flat file.
-        course_tarball = await makeTarFromFlatFile(result, boilerplate_path, makeDownloadLink);
+        course_tarball = await makeTarFromFlatFile(result, boilerplate_structure_path, template, makeDownloadLink);
 
     });
 }
